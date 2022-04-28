d8c223c5fe2eb10a609882b1dc0dcc99d29caeb0
Christopher Fredén <me@icetan.org>
2022-04-28 17:24:13 +0200

    Add usage message and options

--- default.nix
+++ default.nix
@@ -2,14 +2,15 @@
 , ...
 }: with pkgs;
 
+let
+  bins = [ coreutils gnused dasel ];
+in
 runCommand "aifd" {
-  buildInputs = [ bashInteractive makeWrapper  ];
+  buildInputs = [ bashInteractive makeWrapper ] ++ bins;
   execPath = "/bin/aifd";
 } ''
   mkdir -p $out/bin
   cp ${./aifd} $out/bin/aifd
-  wrapProgram $out/bin/aifd --prefix PATH : ${lib.makeBinPath [
-    coreutils gnused dasel
-  ]}
+  wrapProgram $out/bin/aifd --prefix PATH : ${lib.makeBinPath bins}
   patchShebangs $out/bin
 ''
